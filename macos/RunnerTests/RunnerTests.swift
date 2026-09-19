73d9f8e631b7df64b6a906074cdbdb354fdafc1e
michelknoop21 <90530730+michelknoop21@users.noreply.github.com>
2026-09-19 13:13:37 +0200

    test(macos): preserve mpv zoom ownership

--- macos/RunnerTests/RunnerTests.swift
+++ macos/RunnerTests/RunnerTests.swift
@@ -2,7 +2,15 @@ import Cocoa
 import FlutterMacOS
 import XCTest
 
+@testable import Runner
+
 class RunnerTests: XCTestCase {
+
+  func testMacOSKeepsMpvGeometryOwnership() {
+    XCTAssertTrue(MpvPlayerCoreBase.shouldForwardVideoGeometryPropertyToMpv("video-zoom"))
+    XCTAssertTrue(MpvPlayerCoreBase.shouldForwardVideoGeometryPropertyToMpv("panscan"))
+    XCTAssertTrue(MpvPlayerCoreBase.shouldForwardVideoGeometryPropertyToMpv("video-aspect-override"))
+  }
 
   func testExample() {
     // If you add code to the Runner application, consider adding tests here.
