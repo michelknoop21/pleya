0a6229601e673e4bccedbcb6736b239846bdc637
michelknoop21 <90530730+michelknoop21@users.noreply.github.com>
2026-09-19 13:13:35 +0200

    test(ios): lock viewer zoom to the native layer

--- ios/RunnerTests/RunnerTests.swift
+++ ios/RunnerTests/RunnerTests.swift
@@ -2,7 +2,15 @@ import Flutter
 import UIKit
 import XCTest
 
+@testable import Runner
+
 class RunnerTests: XCTestCase {
+
+  func testAppleViewerZoomStaysLayerOwned() {
+    XCTAssertFalse(MpvPlayerCoreBase.shouldForwardVideoGeometryPropertyToMpv("video-zoom"))
+    XCTAssertTrue(MpvPlayerCoreBase.shouldForwardVideoGeometryPropertyToMpv("panscan"))
+    XCTAssertTrue(MpvPlayerCoreBase.shouldForwardVideoGeometryPropertyToMpv("video-aspect-override"))
+  }
 
   func testExample() {
     // If you add code to the Runner application, consider adding tests here.
